19e3aedf6e3c83352a54f6861d6c8ce558ecf468
HETPAHHEP <putinisathief@pm.me>
2023-05-31 02:54:29 +0300

    edit settings_qa.py

--- notesnob_api/notesnob_api/settings_qa.py
+++ notesnob_api/notesnob_api/settings_qa.py
@@ -3,6 +3,6 @@ from notesnob_api.settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
