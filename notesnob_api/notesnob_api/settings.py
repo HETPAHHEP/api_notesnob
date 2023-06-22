e8269d8edfc81951327110650b115c5a726de2b3
HETPAHHEP <putinisathief@pm.me>
2023-06-22 18:49:55 +0300

    fix: edit django secret

--- notesnob_api/notesnob_api/settings.py
+++ notesnob_api/notesnob_api/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(';')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(';')
 
 # Application definition
 
